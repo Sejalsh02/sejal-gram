a8ce3de6f121f902409e60a45f257e9beb7549fa
Sejalsh02 <sejalsharma02@outlook.com>
2026-09-06 18:30:56 +0530

    Fix register API URL

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -10,7 +10,7 @@ form.addEventListener("submit", async (e) => {
 
     try {
 
-        const response = await fetch("http://localhost:5000/api/auth/register", {
+        const response = await fetch("https://sejal-gram.onrender.com", {
 
             method: "POST",
 
